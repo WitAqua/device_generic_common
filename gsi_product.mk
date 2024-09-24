LineageOS/android_device_generic_common

Compile guard aosp apns conf in gsi product
Conflicts with Lineage's BUILD_PREBUILT apns and can't be fixed by include order.

Change-Id: Idb7dca607243fefffe8c16de019a75f26d275ddf

gsi_product.mk
@@ -34,5 +34,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound?=pixiedust.ogg \
     ro.com.android.dataroaming?=true \
 
+ifeq ($(LINEAGE_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif

WitAqua/device_generic_common

gsi_product.mk
@@ -34,5 +34,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound?=pixiedust.ogg \
     ro.com.android.dataroaming?=true \
 
+ifeq ($(WITAQUA_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif